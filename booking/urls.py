a02eca71d5c447414426ae46078d63f53b413b5c
Hritvi Narvekar <hritvi.narvekar@vit.edu.in>
2026-06-13 21:44:13 +0530

    payment done

--- booking/urls.py
+++ booking/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns=[
     path('admin/',admin.site.urls),
-
     path('shows/<int:id>/', views.shows),
-
     path('showseats/<int:id>/', views.showseats),
+    path('bookseat/<int:id>/',views.book),
+    path('payment/<int:id>/', views.payment)
 ]
--- booking/views.py
+++ booking/views.py
@@ -2,45 +2,39 @@ from django.shortcuts import render
 import mysql.connector as m
 import config
 
-db=m.connect(
-    host=config.DB_HOST,
-    user=config.DB_USER,
-    password=config.DB_PASSWORD
-)
+db=m.connect(host=config.DB_HOST,user=config.DB_USER, password=config.DB_PASSWORD)
 
 cursor=db.cursor()
-
 cursor.execute("USE movies")
 
-
 def shows(request,id):
-
-    cursor.execute("""
-        SELECT *
-        FROM showDetails
-        WHERE movie_id=%s
-    """,(id,))
-
+    cursor.execute("""select * from showDetails where movie_id=%s""",(id,))
     shows=cursor.fetchall()
-
-    return render(
-        request,
-        'shows.html',
-        {"shows":shows}
-    )
+    return render(request,'shows.html',{"shows":shows})
 
 def showseats(request,id):
-
-    cursor.execute("""
-        SELECT *
-        FROM seats
-        WHERE show_id=%s
-    """,(id,))
-
+    cursor.execute("""select * from seats where show_id=%s""",(id,))
     seats=cursor.fetchall()
-    print("SEATS =", seats)
-    return render(
-        request,
-        'seats.html',
-        {"seats":seats}
-    )
+    return render(request,'seats.html', {"seats":seats})
+
+def book(request,id):
+    cursor.execute(""" select status from seats where seat_id=%s""",(id,))
+    ava=cursor.fetchone()
+    if(ava[0] != 'available'):
+        return HttpResponse("<p> Sorry this seat is not available </p>")
+    else:
+        cursor.execute(""" select seat_no, seat_type, price from seats where seat_id=%s""",(id,))
+        data=cursor.fetchone()
+
+        return render(request,'payment.html',{"seat_id": id,"seat_no":data[0], "seat_type":data[1],"price":data[2]})
+    
+
+def payment(request,id):
+    cname=request.POST["cname"]
+    cursor.execute(""" select price from seats where seat_id=%s""",(id,))
+    price=cursor.fetchone()
+    cursor.execute(""" insert into payment(seat_id,cname,amt,payment_status) values(%s,%s,%s,%s)
+    """,(id,cname,price[0],'success'))
+    cursor.execute(""" update seats set status='booked' where seat_id=%s """,(id,))
+    db.commit()
+    return render(request,'success.html')
